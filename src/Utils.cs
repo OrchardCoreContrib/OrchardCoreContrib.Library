82dc03f95c17c7dcd9aa6c278a0e8ceeb2a35f20
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-19 22:52:59 +0300

    More docs

--- src/Utils.cs
+++ src/Utils.cs
@@ -14,9 +14,24 @@ public static class Utils
     [Obsolete("Use Greeting() instead.")]
     public static string Greet() => $"Hello, World!";
 
+    /// <summary>
+    /// Creates a personalized greeting message using the specified first and last names.
+    /// </summary>
+    /// <param name="firstName">The first name to include in the greeting. Cannot be null.</param>
+    /// <param name="lastName">The last name to include in the greeting. Cannot be null.</param>
+    /// <returns>A string containing the greeting message formatted as "Hello, {firstName} {lastName}!".</returns>
     public static string Greeting(string firstName, string lastName) => $"Hello, {firstName} {lastName}!";
 
+    /// <summary>
+    /// Returns a personalized greeting message for the specified name.
+    /// </summary>
+    /// <param name="name">The name of the person to include in the greeting. Cannot be null.</param>
+    /// <returns>A string containing a greeting addressed to the specified name.</returns>
     public static string Greeting(string name) => $"Hello, {name}!";
 
+    /// <summary>
+    /// Returns a standard greeting message.
+    /// </summary>
+    /// <returns>A string containing the greeting message "Hello, World!".</returns>
     public static string Greeting() => $"Hello, World!";
 }
