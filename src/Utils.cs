2292246b78ec2d8b2f2e7e374cfd118e8e0387a9
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-19 18:59:40 +0300

    Add Greeting()

--- src/Utils.cs
+++ src/Utils.cs
@@ -7,4 +7,6 @@ public static class Utils
     public static string Greet(string name) => $"Hello, {name}!";
 
     public static string Greet() => $"Hello, World!";
+
+    public static string Greeting() => $"Hello, World!";
 }
