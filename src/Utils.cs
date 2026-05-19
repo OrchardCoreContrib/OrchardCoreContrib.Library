6cb3ffca0d26baacd2536370b4f69bc92557557a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-19 17:23:24 +0300

    Add another overload

--- src/Utils.cs
+++ src/Utils.cs
@@ -2,6 +2,8 @@
 
 public static class Utils
 {
+    public static string Greet(string firstName, string lastName) => $"Hello, {firstName} {lastName}!";
+
     public static string Greet(string name) => $"Hello, {name}!";
 
     public static string Greet() => $"Hello, World!";
