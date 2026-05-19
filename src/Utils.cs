0beb770ba6444c491fab57ce501d78f9c831532f
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-19 19:23:52 +0300

    Deprecate old methods

--- src/Utils.cs
+++ src/Utils.cs
@@ -2,11 +2,18 @@
 
 public static class Utils
 {
+    [Obsolete("Use Greeting(string firstName, string lastName) instead.")]
     public static string Greet(string firstName, string lastName) => $"Hello, {firstName} {lastName}!";
 
+    [Obsolete("Use Greeting(string name) instead.")]
     public static string Greet(string name) => $"Hello, {name}!";
 
+    [Obsolete("Use Greeting() instead.")]
     public static string Greet() => $"Hello, World!";
+
+    public static string Greeting(string firstName, string lastName) => $"Hello, {firstName} {lastName}!";
+
+    public static string Greeting(string name) => $"Hello, {name}!";
 
     public static string Greeting() => $"Hello, World!";
 }
